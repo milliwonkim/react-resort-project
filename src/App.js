b7f50a8e5b8ac137a670ecf457de494a3875c2be
milliwonkim <rldnjs9347@gmail.com>
2020-02-25 23:17:52 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Switch, Route } from "react-router-dom";
+
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
+import Navbar from './components/Navbar';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+      </Switch>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Contentful.js
@@ -0,0 +1,6 @@
+const contentful = require("contentful");
+
+export default contentful.createClient({
+  space: '6pb4z5xu572q',
+  accessToken: 'y4b_lB4SgXJLS6ubgspBaO211SFAxMLLNohH6meo3dE'
+});
